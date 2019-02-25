ba31368b943145829583377363f1025ef1eb8310
nguyenthanhtung2605 <nguyenthanhtung2605@gmail.com>
2019-02-25 12:04:24 -0500

    initial commit

---
+++ c01-basic.js
@@ -0,0 +1,9 @@
+console.log('hello');
+function greeter(person) {
+    return "Hello, " + person;
+}
+var user1 = "vietnd";
+document.body.innerHTML = greeter(user1);
+
+var user2 = "tunghacker";
+document.body.innerHTML = greeter(user2);
---
+++ c01-basic.ts
@@ -0,0 +1,7 @@
+console.log('hello')
+function greeter(person){
+    return "Hello, " + person
+}
+
+var user = "vietnd"
+document.body.innerHTML = greeter(user)
